1be7a0da74e21aaae9547492ebd97ec1dbdf8add
modda <support@moddapampe.de>
2022-06-24 11:10:24 +0200

    final: passed all tests

--- filter-api-data.js
+++ filter-api-data.js
@@ -1,3 +1,16 @@
 function filterApiData(apiData, mandatoryKeys) {
-  // Add you solution here
+let filtered = [];
+for (let data of apiData) {
+  let contain = true;
+  for (let key of mandatoryKeys) {
+    let compare = Object.keys(data);
+    if (compare.includes(key) === false) {
+      contain = false;
+    }
+  }
+  if (contain === true) {
+    filtered.push(data);
+  }
+}
+return filtered;
 }
